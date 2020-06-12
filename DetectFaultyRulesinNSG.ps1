3c41677faec14280edd3b6d03a7d6d03039c65be
Mubarak Tanseer <64589176+mutansee@users.noreply.github.com>
2020-06-12 23:06:04 +0530

    Add files via upload
    DetectFaultyRulesinNSG.ps1: In the world which is run by technology nowadays security plays a significant role in any organization's well being. Its critical to secure our data and resources so that there is no threat. If there is a NSG in the given subscription with the rule set to Source address, Destination address, Source port and Destination port set to 'Any' then it will be a threat to the associated resources. The script helps in identifying such faulty NSGs in a given Azure subscription.

---
+++ DetectFaultyRulesinNSG.ps1
@@ -0,0 +1,22 @@
+﻿Add-AzAccount
+Select-AzSubscription -SubscriptionId ""
+
+
+$RG = ""
+$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName $RG
+$nsgrule = Get-AzNetworkSecurityGroup -ResourceGroupName $RG | Get-AzNetworkSecurityRuleConfig
+$nsgarray = $nsg.Name
+$rulearray = $nsgrule.Name
+
+foreach($nsgitem in $nsgarray)
+{
+$nsg = Get-AzNetworkSecurityGroup -Name $nsgitem
+    foreach ($ruleitem in $rulearray)
+    {
+        $rule = Get-AzNetworkSecurityRuleConfig -Name $ruleitem -NetworkSecurityGroup $nsg
+        if($rule.DestinationPortRange -eq "*" -and $rule.SourcePortRange -eq "*" -and $rule.SourceAddressPrefix -eq "*" -and $rule.DestinationAddressPrefix -eq "*")
+        {
+            write-host Detected faulty rule $rule.Name in $nsg.Name -BackgroundColor DarkRed
+        }
+    }
+}
